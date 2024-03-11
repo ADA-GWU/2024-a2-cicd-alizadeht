c358900537c3487ca2f3abf5dea95d823ab3f769
alizadeht <turalalizadeh4@gmail.com>
2024-03-11 23:02:49 +0400

    Firefox -> Chrome (headless test)

--- src/test/java/ada/edu/demo/webtest/config/SeleniumConfig.java
+++ src/test/java/ada/edu/demo/webtest/config/SeleniumConfig.java
@@ -3,6 +3,7 @@ package ada.edu.demo.webtest.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,13 @@ public class SeleniumConfig {
     @Bean
     public WebDriver webDriver() {
         // Configure Chrome to run in headless mode
-        FirefoxOptions options = new FirefoxOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
 
-        WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver(options);
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(options);
 //        return new FirefoxDriver();
     }
 }
+
+
