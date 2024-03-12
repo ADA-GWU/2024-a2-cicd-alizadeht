988e5815d3aca9c9dd40cc4c9534779a293934b5
alizadeht <turalalizadeh4@gmail.com>
2024-03-12 19:24:47 +0400

    Update SeleniumConfig.java

--- src/test/java/ada/edu/demo/webtest/config/SeleniumConfig.java
+++ src/test/java/ada/edu/demo/webtest/config/SeleniumConfig.java
@@ -4,9 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class SeleniumConfig {
